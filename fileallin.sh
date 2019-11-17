fd0a4601a2b013e96cd0998560cb0639fd23b37f
whitehat92 <ze_correia93@hotmail.com>
2019-11-17 23:02:41 +0000

    Update fileallin.sh

--- fileallin.sh
+++ fileallin.sh
@@ -7,7 +7,7 @@ grep -Eo '(http|https)://[^/"]+' $1;
 #consider special chars
 grep -Eo '*/[A-ZA-z0-9]+/[A-ZA-z0-9]+.*' $1; 
 #grep links with https
-grep "https://" $1 | sed "s/^*https//g" | sed "s/<\/[a-z]>//g" | sed "s/<\/[a-z][a-z][a-z]//g" | sed "s>;
+grep "https://" $1 | sed "s/^*https//g" | sed "s/<\/[a-z]>//g" | sed "s/<\/[a-z][a-z][a-z]//g" | sed "s/<[a-z][a-z]>//g" | sed "s/.*alt*//g" | sed "s/\">//g";
 #script src
 grep -E "<script src" $1;
 #some uri paths
